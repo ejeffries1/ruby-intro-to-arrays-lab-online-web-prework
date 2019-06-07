2db6c9cf466b246b65e6e88b3d9aab2c2f11978f
Evan Jeffries <ejeffries14@gmail.com>
2019-06-07 23:17:39 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+  my_new_array = []
+ return my_new_array
+end
+
+def array_with_two_elements
+  my_two_array = ["wind", "fire"]
+  return my_two_array
+end
+
+def first_element(my_first_element)
+  my_first_element = my_first_element.first
+  return my_first_element
+end
+
+def third_element(my_third_element)
+  my_third_element = my_third_element[2]
+  return my_third_element
+end
+
+def last_element(my_last_element)
+  my_last_element = my_last_element.last
+  return my_last_element
+end
+
+def first_element_with_array_methods(first_country)
+first_country = first_country[0]
+ return first_country
+end
+
+
+
+
